c1fa44080fa3612b73c5e709693e6419412a2d10
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2019-11-08 09:41:56 +0800

    add commandline argparser, add test narcissus

--- main.py
+++ main.py
@@ -5,20 +5,24 @@
 # @Time    : 2019/10/27 13:32
 """
 import os
+import argparse
 from src.ASTBuilder import ASTBuilder
 from src.SymbolTable.SymbolTable import SymbolTable
 
 basics = ["and_or", "arithmetic", "bool", "if", "read_write", "while"]
 bonus = ["break", "continue", "dowhile", "for", "odd", "real", "repeatuntil", "xor"]
-required = ["factorial", "LCM", "prime numbers"]
+program = ["factorial", "LCM", "prime"]
 
 if __name__ == "__main__":
-    test_basic_path = os.getcwd() + r"\tests\tests_basic\test_" + basics[0] + ".txt"
-    test_bonus_path = os.getcwd() + r"\tests\tests_bonus\test_" + bonus[0] + ".txt"
-    test_required_path = os.getcwd() + r"\tests\tests_required" + "\\" + required[0] + ".txt"
-    test_path = os.getcwd() + r"\test.txt"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("type", type=str, help="type in the func kind you want to test")
+    parser.add_argument("test", type=str, help="type in which func you want to test")
+    args = parser.parse_args()
+    test_path = r"C:\Users\robin\Desktop\CX_compiler\tests\tests_" + args.type + r"\test_" + args.test + ".txt"
+    # test_bonus_path = r"C:\Users\robin\Desktop\CX_compiler\tests\tests_bonus\test_" + args.test + ".txt"
+    # test_program_path = r"C:\Users\robin\Desktop\CX_compiler\tests\tests_program" + "\\" + args.test + ".txt"
     symbol_table = SymbolTable()
-    ast = ASTBuilder(test_basic_path, symbol_table)
+    ast = ASTBuilder(test_path, symbol_table)
     ast = ast.build()
     compiled_codes = ast.compile()
     output_path = r"C:\Users\robin\Desktop\CX_compiler\tests\out.p"
--- tests/out.p
+++ tests/out.p
@@ -1,48 +1,70 @@
 ssp 15
 ldc i 0
 str i 0 0
-ldc i 0
-str i 0 4
-ldc i 2
+ldc i 100
 str i 0 0
 bl1:
 lod i 0 0
-ldc i 100
-leq i
-fjp el2
-ldc i 2
-str i 0 4
-bl3:
-lod i 0 4
-lod i 0 0
+ldc i 1000
 les i
-fjp el4
+fjp el2
 lod i 0 0
-lod i 0 0
-lod i 0 4
+ldc i 100
 div i
-lod i 0 4
+str i 0 4
+lod i 0 0
+ldc i 10
+div i
+lod i 0 0
+ldc i 10
+div i
+ldc i 10
+div i
+ldc i 10
 mul i
 sub i
-ldc i 0
-equ i
-fjp l5
-ujp el4
-l5:
-lod i 0 4
-inc i 1
-str i 0 4
-ujp bl3
-el4:
-lod i 0 4
+str i 0 8
 lod i 0 0
+lod i 0 0
+ldc i 10
+div i
+ldc i 10
+mul i
+sub i
+str i 0 12
+lod i 0 4
+ldc i 100
+mul i
+lod i 0 8
+ldc i 10
+mul i
+add i
+lod i 0 12
+add i
+lod i 0 4
+lod i 0 4
+mul i
+lod i 0 4
+mul i
+lod i 0 8
+lod i 0 8
+mul i
+lod i 0 8
+mul i
+add i
+lod i 0 12
+lod i 0 12
+mul i
+lod i 0 12
+mul i
+add i
 equ i
-fjp l7
+fjp l3
 lod i 0 0
 out i
 ldc c '\n'
 out c
-l7:
+l3:
 lod i 0 0
 inc i 1
 str i 0 0
